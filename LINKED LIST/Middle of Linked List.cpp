7e93d75c27735bf0a452e1cd4a4fcaf8c602f80f
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-08-04 20:55:53 +0000

    using slow and fast pointer

---
+++ LINKED LIST/Middle of Linked List.cpp
@@ -0,0 +1,18 @@
+//    **** using HARE and TORTOISE******
+// using slow and fast pointer
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head ;
+        ListNode* fast = head;
+
+        while(fast!=NULL && fast->next!= NULL)
+        {
+            slow = slow->next ;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
