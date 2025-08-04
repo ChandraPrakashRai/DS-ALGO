b0b7b5651422f24d308e54698ac41c1e522d3a17
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-08-04 20:41:23 +0000

    easy way to merge 2 sorted linked list

---
+++ LINKED LIST/Merge 2 Sorted Linked List.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+        //  very easy way to merge two sorted linked list
+    
+    
+class Solution {
+public:
+
+    ListNode* solve(ListNode* list1 , ListNode* list2)
+    {
+        ListNode* result;
+        if(list1==NULL) return list2;
+        if(list2==NULL) return list1;
+
+        if(list1->val < list2->val)
+        {
+            result = list1;
+            result->next = solve(list1->next , list2);
+        }
+        else 
+        {
+            result = list2;
+            result->next = solve(list1 , list2->next);
+        }
+
+        return result;
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+
+        return solve(list1  , list2);
+        
+    }
+};
